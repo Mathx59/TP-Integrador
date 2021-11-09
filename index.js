ffe32656ed03a94fe59357d255898331a4683a5f
Mathx59 <matias.sandoval82@gmail.com>
2021-11-09 20:15:39 -0300

    TP Integrador

---
+++ index.js
@@ -0,0 +1,83 @@
+// desactiva loader spinner
+const carga = document.getElementById("carga");
+
+window.addEventListener("load", function () {
+    carga.style.display = "none";
+    
+});
+
+
+
+
+
+//menu hamburguesa
+const links = document.querySelectorAll(".menuLinks li a");
+const ham = document.querySelector('.ham');
+const enlaces = document.querySelector('.menuLinks');
+const barras = document.querySelectorAll('.ham span');
+
+
+
+ham.addEventListener('click', () => {
+    enlaces.classList.toggle('activado');
+    barras.forEach(child => {child.classList.toggle('animado')});
+    ham.classList.toggle('girar');
+});
+
+links.forEach((Element) => {
+    Element.addEventListener("click", () => {
+        enlaces.classList.toggle('activado')
+    });
+});
+
+
+
+
+// Efecto carrousel de cards
+$(function () {
+    $('.center').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 3900,
+        dots: false,
+        centerModel: true,
+        centerPadding: '60px',
+        responsive: [
+            {
+                breakpoint: 1440,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    
+                }
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
